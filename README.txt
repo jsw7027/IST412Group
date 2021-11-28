0c3c2a890a1a81f9377fd2a58f044616c64281a0
DJO5115 <odonnelld92@outlook.com>
2021-11-27 23:05:41 -0500

    Updated Employee class and added README TXT file.

---
+++ README.txt
@@ -0,0 +1,29 @@
+1.  (Seung Jung / Daniel O'Donnell) Issue: The model class "customer" is empty and does not do anything.
+    Solution:
+
+2.  (Daniel O'Donnell) Issue: The model class "Employee" is empty and does not do anything.
+    Solution: The Employee class was revised to include attributes with encapsulation that includes getters/setters.
+
+3.  (David Hernandez) Issue: UserController class is the longest Java class that we have at 123 lines.
+    Solution:
+
+4.  (Daniel O'Donnell) Issue: There are no comments in the code
+    Solution:
+
+5.  (Daniel O'Donnell) Issue: The model class "LoanApplication" lacks logical statements to retrieve various loan app types.
+    Solution:
+
+6.  (David Hernandez) Issue: The service class "LoanServiceImpt" has many methods.
+    Solution:
+
+7.  (David Hernandez) Issue: The service class "UserServiceImpl" has many methods.
+    Solution:
+
+8.  (Seung Jung) Issue: The html code for Customer Screen does not do anything productive except display a title page but no content.
+    Solution:
+
+9.  (Jennifer Lewis) Issue: Easy to have access to Employee dashboard.
+    Solution:
+
+10. (Jennifer Lewis) Issue: Employee dashboard doesn't display the "customers" that have signed up.
+    Solution:
--- src/main/java/com/example/financeapp/model/Employee.java
+++ src/main/java/com/example/financeapp/model/Employee.java
@@ -1,4 +1,68 @@
 package com.example.financeapp.model;
 
 public class Employee {
+
+private String firstName;
+private String lastName;
+private String streetAddress;
+private String city;
+private String state;
+private int zipCode;
+private int phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
